9ce908695e96ddba8f50e5efadeb957bc89dbb26
Ricardo Ferreira <calditos1988@outlook.com>
2021-03-08 22:37:33 -0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        
+        <link rel="stylesheet" href="/css/styles.css">
+        <script src="/js/scripts.js"></script>
+    </head>
+    <body>
+          <h1>Imoveis web</h1>
+          <img src="/img/banner.jpg" alt="Banner">
+       
+    </body>
+</html>

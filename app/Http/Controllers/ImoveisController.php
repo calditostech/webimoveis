627f8b76ed105a04ceb4b7734e08138c17086ae2
Ricardo Ferreira <calditos1988@outlook.com>
2021-03-10 22:19:14 -0300

    finalizando a home

--- app/Http/Controllers/ImoveisController.php
+++ app/Http/Controllers/ImoveisController.php
@@ -3,14 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Imovel;
 
 class ImoveisController extends Controller
 {
     public function index(){
 
+        $imoveis = Imovel::all();
+
+        return view('welcome', ['imoveis' => $imoveis]);
+
     }
 
     public function create(){
+        
         return view('imoveis.create');
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,3 +6,27 @@
 
 <h1>Imoveis web</h1>
 
+  <div id="search-container" class="col-md-12">
+     <h1>Busque um imóvel</h1>
+     <form action="">
+         <input type="text" id="search" name="search" class="form-control" placeholder="Procure um imóvel..">
+     </form>
+  </div>
+  <div id="imoveis-container" class="col-md-12">
+      <h2>Próximos Eventos</h2>
+      <p>Veja os eventos dos proximos dias</p>
+      <div id="cards-container" class="row">
+      @foreach($imoveis as $imovel)
+        <div class="card-col-md-3">
+            <img src="/img/banner.jpg" alt="{{$imovel->titulo}}">
+            <div class="card-body">
+            <p class="card-date">10/09/2021</p>
+            <h5 class="card-title">{{$imovel->titulo}}</h5>
+            <p class="card-participantes">x participantes</p>
+            <a href="#" class="btn btn-primary">Saber mais</a>
+        </div>
+  </div>
+@endforeach
+</div>
+@endsection
+
